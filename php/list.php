08b5bdea43768b8c343a811f81f46aaff328a18e
Rio Purnomo <siriokun@gmail.com>
2021-11-25 20:40:25 +0700

    fix collum name

--- php/list.php
+++ php/list.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 include "./db.php";
-$stmt = $db->prepare("SELECT id, name, age FROM student");
+$stmt = $db->prepare("SELECT kode, name, sks, semester, dosen FROM jadwal");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($result);
